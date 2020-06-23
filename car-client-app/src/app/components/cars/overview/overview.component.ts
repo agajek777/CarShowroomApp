55bc81b34941065a07f8b822a09a2c209cc9884f
agajek777 <adam.gajek777@gmail.com>
2020-06-23 17:59:50 +0200

    Add Routing

--- car-client-app/src/app/components/cars/overview/overview.component.ts
+++ car-client-app/src/app/components/cars/overview/overview.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Car } from "../../../models/car";
+import { HttpService } from "../../../services/http.service";
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css']
 })
-export class OverviewComponent implements OnInit {
+export class OverviewComponent {
+  public cars: Car[];
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
 
-  ngOnInit(): void {
+  public getCars() {
+    let route: string = "https://localhost:44332/car/";
+    this.httpService.getData(route)
+      .subscribe((result) => {
+        this.cars = result as Car[];
+      },
+      (error) => {
+        console.error(error);
+      });
   }
-
 }
---
+++ car-client-app/src/app/models/car.ts
@@ -0,0 +1,11 @@
+export interface Car {
+  brand: string,
+  model: string,
+  engine: string,
+  power: number,
+  production: string,
+  price: number,
+  imagePath: string,
+  description: string,
+  mileage: number
+}
