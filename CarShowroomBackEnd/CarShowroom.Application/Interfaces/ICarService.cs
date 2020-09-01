804c6f28fd875b02935e4e731aafa069fe6766c1
agajek777 <adam.gajek777@gmail.com>
2020-09-01 23:40:19 +0200

    Add CarService with interface

---
+++ CarShowroomBackEnd/CarShowroom.Application/Interfaces/ICarService.cs
@@ -0,0 +1,18 @@
+﻿using CarShowroom.Domain.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowroom.Application.Interfaces
+{
+    public interface ICarService
+    {
+        public IEnumerable<CarDto> GetAllCars();
+        public Task<CarDto> GetCar(int id);
+        public Task<CarDto> AddCar(CarDto carToAdd);
+        public Task<CarDto> UpdateCar(int id, CarDto carToUpdate);
+        public Task<IActionResult> DeleteCar(int id);
+    }
+}
