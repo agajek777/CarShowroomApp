fc195330b73e7f639d85107ea9633bde7c370927
agajek777 <adam.gajek777@gmail.com>
2020-10-04 15:24:47 +0200

    Update ConfigureServices

--- CarShowroomBackEnd/CarShowroomApp.UI/Startup.cs
+++ CarShowroomBackEnd/CarShowroomApp.UI/Startup.cs
@@ -53,9 +53,9 @@ namespace CarShowroomApp
             services.AddCors(options =>
             {
                 options.AddPolicy("CorsPolicy",
-                    builder => builder.WithOrigins("http://localhost:4200")
+                    builder => builder.AllowAnyOrigin()
                     .AllowAnyMethod()
-                    .AllowAnyHeader().AllowCredentials());
+                    .AllowAnyHeader());
             });
 
             services.AddIdentity<User, Role>()
