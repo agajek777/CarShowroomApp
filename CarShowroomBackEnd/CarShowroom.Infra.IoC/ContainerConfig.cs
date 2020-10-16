121e47296a90c0b0f6fe461b4b99f398cecbdcb3
agajek777 <adam.gajek777@gmail.com>
2020-10-16 11:44:31 +0200

    Add Assembly Scanning

--- CarShowroomBackEnd/CarShowroom.Infra.IoC/ContainerConfig.cs
+++ CarShowroomBackEnd/CarShowroom.Infra.IoC/ContainerConfig.cs
@@ -14,17 +14,14 @@ namespace CarShowroom.Infra.IoC
     {
         public static void Configure(ContainerBuilder builder)
         {
-            // CarShowroom.Domain
-            builder.RegisterType<CarRepository>().AsImplementedInterfaces();
-            builder.RegisterType<MessageRepository>().AsImplementedInterfaces();
-            builder.RegisterType<MessageHub>().AsImplementedInterfaces();
+            var assemblyDomain = Assembly.LoadFrom($"..\\{nameof(CarShowroom)}.{nameof(Domain)}\\bin\\Debug\\netcoreapp3.1\\{nameof(CarShowroom)}.{nameof(Domain)}.dll");
+            builder.RegisterAssemblyTypes(assemblyDomain).Where(t => t.Namespace.EndsWith(nameof(Domain.Models.Messaging))).AsImplementedInterfaces();
 
-            // CarShowroom.Domain
-            builder.RegisterType<CarService>().AsImplementedInterfaces();
-            builder.RegisterType<MessageService>().AsImplementedInterfaces();
-            builder.RegisterType<JwtService>().AsImplementedInterfaces();
+            var assemblyInfraData = Assembly.LoadFrom($"..\\{nameof(CarShowroom)}.{nameof(Infra)}.{nameof(Data)}\\bin\\Debug\\netcoreapp3.1\\{nameof(CarShowroom)}.{nameof(Infra)}.{nameof(Data)}.dll");
+            builder.RegisterAssemblyTypes(assemblyInfraData).Where(t => t.Namespace.EndsWith(nameof(Data.Repositories))).AsImplementedInterfaces();
 
-            builder.RegisterType<ResponseCacheService>().AsImplementedInterfaces();
+            var assemblyApplication = Assembly.LoadFrom($"..\\{nameof(CarShowroom)}.{nameof(Application)}\\bin\\Debug\\netcoreapp3.1\\{nameof(CarShowroom)}.{nameof(Application)}.dll");
+            builder.RegisterAssemblyTypes(assemblyApplication).Where(t => t.Namespace.EndsWith(nameof(Application.Services))).AsImplementedInterfaces();
         }
     }
 }
