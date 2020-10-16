208e2a612e0b5f71dc8ed16ec5fd9fd786091069
agajek777 <adam.gajek777@gmail.com>
2020-10-17 01:07:47 +0200

    Add Repository class

---
+++ CarShowroomBackEnd/CarShowroom.Infra.Data/Repositories/Model/Repository.cs
@@ -0,0 +1,27 @@
+﻿using CarShowroom.Domain.Models.Identity;
+using CarShowroom.Infra.Data.Context;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace CarShowroom.Infra.Data.Repositories.Model
+{
+    public abstract class Repository
+    {
+        private async Task<bool> CheckConnectionAsync(DatabaseContext<User, Role> _db, ILogger<Repository> _logger)
+        {
+            try
+            {
+                await _db.GetService<IRelationalDatabaseCreator>().CanConnectAsync();
+            }
+            catch (SqlException ex)
+            {
+                _logger.LogWarning(ex, "CheckConnectionAsync() got exception: {Message}", ex.Message);
+                return false;
+            }
+            return true;
+        }
+    }
+}
