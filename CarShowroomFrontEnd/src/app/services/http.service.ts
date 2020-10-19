1b5bf8b6934c9c46815be6116317f3e10a796cfb
agajek777 <adam.gajek777@gmail.com>
2020-10-19 22:42:46 +0200

    Add register abd login angular service

--- CarShowroomFrontEnd/src/app/services/http.service.ts
+++ CarShowroomFrontEnd/src/app/services/http.service.ts
@@ -6,22 +6,41 @@ import { Car } from '../models/car';
   providedIn: 'root'
 })
 export class HttpService {
-  private route: string = "https://localhost:44332/api/car/";
+  private apiCarRoute: string = "https://localhost:44332/api/car/";
+  private apiUserRoute: string = "https://localhost:44332/api/user/";
 
   constructor(private httpClient: HttpClient) { }
 
   public getData(id?: string) {
     if (typeof(id) === 'undefined' || id === null || id === '') {
-      return this.httpClient.get(this.route);
+      return this.httpClient.get(this.apiCarRoute);
     }
-    return this.httpClient.get(this.route + id);
+    return this.httpClient.get(this.apiCarRoute + id);
   }
 
   public addData(body: Car) {
-    return this.httpClient.post(this.route, body);
+    return this.httpClient.post(this.apiCarRoute, body);
   }
 
   public deleteData(id: number) {
-    return this.httpClient.delete(this.route + id);
+    return this.httpClient.delete(this.apiCarRoute + id);
+  }
+
+  public register(username: string, password: string) {
+    return this.httpClient.post(this.apiUserRoute + 'register',
+      {
+        userName: username,
+        password: password
+      }
+    )
+  }
+
+  public login(username: string, password: string) {
+    return this.httpClient.post(this.apiUserRoute + 'login',
+      {
+        userName: username,
+        password: password
+      }
+    )
   }
 }
