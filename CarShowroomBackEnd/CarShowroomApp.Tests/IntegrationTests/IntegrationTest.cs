f9c78c7c39f3c658728c522634de10ac64560d08
Adam Gajek <adam.gajek777@gmail.com>
2021-02-06 18:51:11 +0100

    add folders add base class

---
+++ CarShowroomBackEnd/CarShowroomApp.Tests/IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,16 @@
+﻿using CarShowroomApp;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+
+namespace CarShowroom.UI.Tests.IntegrationTests
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+        public IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            TestClient = appFactory.CreateClient();
+        }
+    }
+}
